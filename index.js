9fcd41fb05c3736ea2ab573daab4490020799f0a
Molly <mollymccollumwx@gmail.com>
2021-03-12 12:00:46 -0500

    added reverse int problem

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// PROBLEM: Create a function to find the index of the random value 
+// PROBLEM: Create a function to find the index of the random value
 
-var stuff = [10,34,56,67,93,120,137,168,259,280,311,342,413,514];
+var stuff = [10, 34, 56, 67, 93, 120, 137, 168, 259, 280, 311, 342, 413, 514];
 
-var random_value = stuff[ Math.floor( Math.random() * 14 ) ];
+var random_value = stuff[Math.floor(Math.random() * 14)];
 
 // SOLUTION:
 
-function findRandom () {
-
-    for(let i = 0; i < stuff.length; i++) {
-        if(stuff[i] === random_value){
-            console.log(i);
-        }
+function findRandom() {
+  for (let i = 0; i < stuff.length; i++) {
+    if (stuff[i] === random_value) {
+      console.log(i);
     }
+  }
 }
 
 findRandom();
 
-// EXPLANATION: Linear Search 
+// EXPLANATION: Linear Search
 
 // ========================================================================= //
 
@@ -25,7 +24,7 @@ findRandom();
 
 // SOLUTION: 1
 function reverse(str) {
- return str.split('').reverse().join('');
+  return str.split("").reverse().join("");
 }
 
 // EXPLANATION: split turns it into an array, reverse reverses the content of the array, join turns it back into a string
@@ -33,62 +32,68 @@ function reverse(str) {
 // SOLUTION: 2
 
 function reverse(str) {
- let reversed = '';
+  let reversed = "";
 
- for (let character of str) {
-     reversed = character + reversed;
- }
+  for (let character of str) {
+    reversed = character + reversed;
+  }
 
- return reversed;
-
+  return reversed;
 }
 
-// SOLUTION: 3 
+// SOLUTION: 3
 
 function reverse(str) {
-    return str.split('').reduce((reversed, character)=> {
-       return character + reversed;
-    }, '');
+  return str.split("").reduce((reversed, character) => {
+    return character + reversed;
+  }, "");
 }
 
 // EXPLANATION: .reduce() takes all the content of an array and reduces it to one singular value
 
 // ====================================================================== //
 
-// PROBLEM: Palindrome. Find out if a string is a palindrome or not. 
+// PROBLEM: Palindrome. Find out if a string is a palindrome or not.
 
 // SOLUTION # 1:
 function palindrome(str) {
-    if (str === str.split('').reverse().join('')){
-        return true;
-    } else {
-        return false;
-    }
-
+  if (str === str.split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 // manually reversed the string and compare it to the original string
 
 // SOLUTION #2:
 
 function palindrome(str) {
-return str.split('').every((char, i)=> {
-    return char === str[str.length -i - 1]
- });
-
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 // .every() allows you to check every element in an array and make it pass a test
-// not an ideal solution because you are doing my checks that 
+// not an ideal solution because you are doing my checks that
 
 // ================================================================== //
-
 
 // PROBLEM: Reversing an Integer. Ex: reverseInt(15) === 51
 
 function reverseInt(n) {
+   
+  
+  const reversed = n.toString().split("").reverse().join("");
 
 
-
+  return parseInt(reversed) * Math.sign(n);
 }
 
 // toString() turns an integer to a string, parseInt() turns string to integer
+// Math.sign() indicates whether is positive (1) or negative (-1)
+
+// ====================================================================//
+
+// PROBLEM: MAX CHAR. Ex: maxChar("abccccccccd") === "c". Return the character used most inside the string
+
+
